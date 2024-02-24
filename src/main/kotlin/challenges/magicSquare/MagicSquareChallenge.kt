8e29c61c0cca927465968b8a3d8da0f5c1553b35
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-02-23 21:43:20 -0300

    first MagicSquareChallenge

--- src/main/kotlin/challenges/magicSquare/MagicSquareChallenge.kt
+++ src/main/kotlin/challenges/magicSquare/MagicSquareChallenge.kt
@@ -1,20 +1,44 @@
 package challenges.magicSquare
 
 import challenges.base.BaseChallenge
+import kotlin.math.abs
+
+private const val MAGIC_SIZE = 15
 
 class MagicSquareChallenge(
     input: Array<Array<Int>>
 ) : BaseChallenge<Array<Array<Int>>, Int>(input) {
     override fun execute(): Int {
 
-        val mergeInput = mergeInput(input)
-        println(mergeInput)
+        val see = mutableListOf<Int>()
+        val repeated = mutableListOf<Int>()
 
+        input.forEachIndexed { i, ints ->
+            // linha repete?
+            val repeat = checkLine(ints)
+            // linha tem 15?
+            val sum = abs(MAGIC_SIZE - ints.sum())
+            repeatedNumbers(ints, see, repeated)
+
+        }
         return 0
     }
 
-    private fun mergeInput(input: Array<Array<Int>>): List<Int> {
-        return input[0].plus(input[1]).plus(input[2]).sorted()
+    private fun repeatedNumbers(
+        numbers: Array<Int>,
+        see: MutableList<Int>,
+        repeated: MutableList<Int>
+    ) {
+        numbers.forEach { number ->
+            if (see.contains(number))
+                repeated.add(number)
+            else
+                see.add(number)
+        }
+    }
+
+    private fun checkLine(input: Array<Int>): Boolean {
+        return input.distinct().size == input.size
     }
 
 }
